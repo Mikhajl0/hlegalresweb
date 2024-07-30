ad3ac914304dd21940b7ee1d4365febf86756e91
Mikhajlo <petrovmikhajlo@gmail.com>
2024-07-30 18:50:36 +0300

    creating a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const clean = require("gulp-clean");
+const autoprefixer = require("autoprefixer");
+const postcss = require("gulp-postcss");
+const htmlmin = require("gulp-htmlmin");
+const browserSync = require('browser-sync').create();
+
+function css() {
+  return gulp
+    .src("./src/sass/style.scss")
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("./build"));
+    // .pipe(browserSync.stream());
+}
+
+function clear() {
+  return gulp.src("build", { read: false, allowEmpty: true }).pipe(clean());
+}
+
+function watching() {
+  gulp.watch("./src/sass/*.scss", css);
+  gulp.watch("./src/*.html", html).on("change", browserSync.reload);
+  gulp.watch('./src/assets/**/*', copy).on('change', browserSync.reload);
+}
+
+function html() {
+  return gulp
+    .src("./src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./build"));
+}
+
+function copy() {
+  return gulp.src("./src/assets/**/*", { encoding: false,}).pipe(gulp.dest("./build"))
+}
+
+function server() {
+  browserSync.init({
+    server: {
+      baseDir: "./build/"
+    },
+  });
+}
+
+exports.start = gulp.series(
+  clear,
+  gulp.parallel(css, html, copy),
+  gulp.parallel(watching, server)
+);
